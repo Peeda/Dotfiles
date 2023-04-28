5f5102b49a4ed53060e736475aebea3da938c206
Peeda <107806301+Peeda@users.noreply.github.com>
2023-04-28 16:20:16 -0400

    configure cmp

--- nvchad/.config/nvim/lua/custom/plugins.lua
+++ nvchad/.config/nvim/lua/custom/plugins.lua
@@ -5,6 +5,18 @@ local plugins = {
         require "plugins.configs.lspconfig"
         require "custom.configs.lspconfig"
     end,
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        enabled = true,
+        opts = {
+            sources = {
+                {name = 'luasnip', keyword_length = 2},
+                {name = 'path'},
+                {name = 'buffer', keyword_length = 3},
+                {name = 'nvim_lsp', keyword_length = 5},
+            },
+        },
     }
 }
 return plugins
