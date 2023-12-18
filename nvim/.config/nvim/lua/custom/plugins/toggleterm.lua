4775f0919f99f3178e79405bea94d68be1543b2d
Peter <peterchen4321@icloud.com>
2023-12-18 17:49:38 -0500

    add toggleterm

---
+++ nvim/.config/nvim/lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,10 @@
+return {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    opts = {
+        open_mapping = [[<c-/>]],
+        close_on_exit = true,
+        direction = "float",
+        insert_mappings = true,
+    },
+}
